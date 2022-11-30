4523307975bf1dee56aef0b975aa782244b622a6
tomer <tomertal52@yahoo.com>
2022-11-29 23:03:11 -0500

    commit tree e6e643445607e341de221410fbc8af0aa9ac4d2e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+
+function submitData(name, email){
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then(user => document.body.append(user.id))
+    .catch(err => document.body.innerHTML = err.message)
+}
+
+submitData("Jim", "jim@jim.com");
